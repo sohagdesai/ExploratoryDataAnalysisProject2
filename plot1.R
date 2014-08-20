16d6b10e6bad9ab59ae98b7c22460543ae108858
Sohag Desai <sohag.desai@gmail.com>
2014-08-20 15:41:08 -0700

    Added comments; cleaned up code

--- plot1.R
+++ plot1.R
@@ -1,17 +1,33 @@
 library(graphics)
 plot1 <- function() {
+        # Read in data
         NEI <- readRDS("summarySCC_PM25.rds")
+        
+        # Find total emissions for each year of interest
         s99 <- sum(NEI$Emissions[NEI$year == 1999])
         s02 <- sum(NEI$Emissions[NEI$year == 2002])
         s05 <- sum(NEI$Emissions[NEI$year == 2005])
         s08 <- sum(NEI$Emissions[NEI$year == 2008])
-        NEI99 <- c("Total.Emissions" = as.numeric(s99),"year" = as.integer("1999"))
-        NEI02 <- c("Total.Emissions" = as.numeric(s02),"year" = as.integer("2002"))
-        NEI05 <- c("Total.Emissions" = as.numeric(s05),"year" = as.integer("2005"))
-        NEI08 <- c("Total.Emissions" = as.numeric(s08),"year" = as.integer("2008"))
-        NEIsummary <- rbind(NEI99,NEI02,NEI05,NEI08)
+        
+        # Create vectors for each year
+        NEI99 <- c("Total.Emissions" = as.numeric(s99),
+                   "year" = as.integer("1999"))
+        NEI02 <- c("Total.Emissions" = as.numeric(s02),
+                   "year" = as.integer("2002"))
+        NEI05 <- c("Total.Emissions" = as.numeric(s05),
+                   "year" = as.integer("2005"))
+        NEI08 <- c("Total.Emissions" = as.numeric(s08),
+                   "year" = as.integer("2008"))
+        
+        # Create a table with these four vectors
+        NEIsummary <- rbind(NEI99, NEI02, NEI05, NEI08)
+        
+        # Make a barplot to a PNG file
         png(filename = "plot1.png")
         options(scipen=10)
-        barplot(NEIsummary[,"Total.Emissions"],names=NEIsummary[,"year"],xlab="Year",ylab="Total Emmissions")
+        barplot(NEIsummary[,"Total.Emissions"],
+                names=NEIsummary[,"year"],
+                xlab="Year",
+                ylab="Total Emmissions")
         dev.off()
 }
--- plot2.R
+++ plot2.R
@@ -1,17 +1,36 @@
 library(graphics)
 plot2 <- function() {
+        # Read in data
         NEI <- readRDS("summarySCC_PM25.rds")
+        
+        # Find total emissions for each year of interest for city of interest (Baltimore City)
         sBaltCity99 <- sum(NEI$Emissions[NEI$year == 1999 & NEI$fips == 24510])
         sBaltCity02 <- sum(NEI$Emissions[NEI$year == 2002 & NEI$fips == 24510])
         sBaltCity05 <- sum(NEI$Emissions[NEI$year == 2005 & NEI$fips == 24510])
         sBaltCity08 <- sum(NEI$Emissions[NEI$year == 2008 & NEI$fips == 24510])
-        NEIBaltCity99 <- c("Total.Emissions" = as.numeric(sBaltCity99),"year" = as.integer("1999"))
-        NEIBaltCity02 <- c("Total.Emissions" = as.numeric(sBaltCity02),"year" = as.integer("2002"))
-        NEIBaltCity05 <- c("Total.Emissions" = as.numeric(sBaltCity05),"year" = as.integer("2005"))
-        NEIBaltCity08 <- c("Total.Emissions" = as.numeric(sBaltCity08),"year" = as.integer("2008"))
-        NEIBaltCitysummary <- rbind(NEIBaltCity99,NEIBaltCity02,NEIBaltCity05,NEIBaltCity08)
+        
+        # Create vectors for each year
+        NEIBaltCity99 <- c("Total.Emissions" = as.numeric(sBaltCity99),
+                           "year" = as.integer("1999"))
+        NEIBaltCity02 <- c("Total.Emissions" = as.numeric(sBaltCity02),
+                           "year" = as.integer("2002"))
+        NEIBaltCity05 <- c("Total.Emissions" = as.numeric(sBaltCity05),
+                           "year" = as.integer("2005"))
+        NEIBaltCity08 <- c("Total.Emissions" = as.numeric(sBaltCity08),
+                           "year" = as.integer("2008"))
+        
+        # Create a table with these four vectors
+        NEIBaltCitySummary <- rbind(NEIBaltCity99,
+                                    NEIBaltCity02,
+                                    NEIBaltCity05,
+                                    NEIBaltCity08)
+        
+        # Make a barplot to a PNG file
         png(filename = "plot2.png")
         options(scipen=10)
-        barplot(NEIBaltCitySummary[,"Total.Emissions"],names=NEIBaltCitySummary[,"year"],xlab="Year",ylab="Total Emmissions for Baltimore City")
+        barplot(NEIBaltCitySummary[,"Total.Emissions"],
+                names=NEIBaltCitySummary[,"year"],
+                xlab="Year",
+                ylab="Total Emmissions for Baltimore City")
         dev.off()
 }
